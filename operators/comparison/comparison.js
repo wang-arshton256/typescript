ae4cfe1518524637a14255aa6a76a3bfa0447851
wang-arshton256 <wangarshton@gmail.com>
2026-02-18 12:53:19 +0300

    comparison

---
+++ operators/comparison/comparison.js
@@ -0,0 +1,7 @@
+var x = 10;
+var y = 250;
+console.log(x === 25); // Equal to
+console.log(x !== 75); // Not equal to
+console.log(x < 100); // Less than
+console.log(x > y); // Greater than
+console.log(x > y); // Not greater than
---
+++ operators/comparison/comparison.ts
@@ -0,0 +1,9 @@
+var x: number = 10;
+var y: number = 250;
+
+
+console.log(x === 25);  // Equal to
+console.log(x !== 75);  // Not equal to
+console.log(x < 100);  // Less than
+console.log(x > y);  // Greater than
+console.log(x !> y);  // Not greater than
