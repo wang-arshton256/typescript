b070394e8e5ff6e5bc9767e23e2cb9ca17a2feb7
wang-arshton256 <wangarshton@gmail.com>
2026-02-18 12:52:12 +0300

    assignmentn operators

---
+++ operators/assignment/assignment.js
@@ -0,0 +1,5 @@
+var num1 = 25;
+var num2 = 75;
+var num3 = num2;
+num3 = +num1;
+console.log(num3);
---
+++ operators/assignment/assignment.ts
@@ -0,0 +1,6 @@
+var num1: number = 25;
+var num2: number = 75;
+var num3: number = num2; 
+num3 = + num1;
+
+console.log(num3);
