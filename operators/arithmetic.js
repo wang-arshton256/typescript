962c74b70a144af5ac853487341ff6011b3f70bf
wang-arshton256 <wangarshton@gmail.com>
2026-02-18 12:12:03 +0300

    operators

---
+++ operators/arithmetic.js
@@ -0,0 +1,7 @@
+var x = 10;
+var y = 50;
+console.log(x + y); // addition
+console.log(x - y); // subtraction
+console.log(x * y); // multiplication
+console.log(x / y); // division
+console.log(x % y); // remainder
