f497e7265b2386a9bffa1cd4ce15a78711725b65
wang-arshton256 <wangarshton@gmail.com>
2026-02-20 14:51:11 +0300

    array methods

---
+++ object-arrays/arrays/array-methods.js
@@ -0,0 +1,7 @@
+var levels = [20, 30, 45, 98, 65, 741];
+console.log(levels.toString); // Changes to string
+console.log(levels.join(" ")); // Joins entries
+console.log(levels.slice(3)); // Deletes starting from index of 3
+console.log(levels.toString());
+levels.splice(2, 3, 5, 965); // Deletes and asigns ew values to array
+console.log(levels.toString());
