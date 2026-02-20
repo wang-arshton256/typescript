2d8492b567e7d5d3e5ee564adcaa6cc7a4421a42
wang-arshton256 <wangarshton@gmail.com>
2026-02-20 12:38:59 +0300

    objects

---
+++ object-arrays/objects.ts
@@ -0,0 +1,10 @@
+var student = {
+    name: "Wang",
+    gender: "Male",
+    age: 20,
+    tribe: "Gishu"
+}
+console.log(student);
+console.log(student.age);
+console.log(student.gender);
+console.log(student.tribe);
