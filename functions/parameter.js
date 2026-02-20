96fe26ee9a6ea7e84255d98ec8bcec901fd89548
wang-arshton256 <wangarshton@gmail.com>
2026-02-20 15:46:28 +0300

    call back functions

--- functions/parameter.js
+++ functions/parameter.js
@@ -3,3 +3,27 @@ function hello(name) {
     ;
 }
 console.log(hello("Wangutusi"));
+// fuction to ad numbers
+function add(num1, num2) {
+    return num1 + num2;
+}
+console.log("The sum is " + add(789, 65)); // Invoking the function
+// Optional parameters
+function display(id, name, role) {
+    console.log("Id:", id);
+    console.log("Name", name);
+    if (role != undefined) {
+        console.log("Role", role);
+    }
+}
+display(1, "Wangjtusi", "Admin");
+// Default parameters
+function display1(id, name, role) {
+    console.log("Id:", id);
+}
+display(1, "Wangjtusi", "Admin");
+//function as a parameter of another
+function calculator(fun) {
+    console.log(fun(10, 1));
+}
+calculator(add);
--- functions/parameter.ts
+++ functions/parameter.ts
@@ -2,3 +2,37 @@ function hello(name:string): string{
     return "Hello" + name;;
 }
 console.log(hello("Wangutusi"));
+
+// fuction to ad numbers
+function add(num1: number, num2: number): number{
+    return num1 + num2;
+}
+
+console.log("The sum is " + add(789, 65)); // Invoking the function
+
+// Optional parameters
+function display(id: number, name: string, role?: string) {
+    console.log("Id:", id);
+    console.log("Name", name);
+    if (role != undefined) {
+         console.log("Role", role);
+    } 
+}
+
+display(1, "Wangjtusi", "Admin");
+
+// Default parameters
+function display1(id: number, name: string, role: "Normal") {
+    console.log("Id:", id);
+    
+}
+
+display(1, "Wangjtusi", "Admin");
+
+
+//function as a parameter of another(Call back Function)
+function calculator(fun: any): void{
+    console.log(fun(10, 1));
+}
+
+calculator(add);
