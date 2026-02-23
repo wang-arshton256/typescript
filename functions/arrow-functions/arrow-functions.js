0a7eb9fa2576a8063c3716aa2860c9e9f3dc73f7
wang-arshton256 <wangarshton@gmail.com>
2026-02-23 11:33:26 +0300

    passing parameters

--- functions/arrow-functions/arrow-functions.js
+++ functions/arrow-functions/arrow-functions.js
@@ -2,3 +2,8 @@ var greeting = function () {
     return "Hello";
 };
 console.log(greeting());
+// Passing Parameters
+var profile = function (name) {
+    return "Wangutusi Arshton";
+};
+console.log(profile());
--- functions/arrow-functions/arrow-functions.ts
+++ functions/arrow-functions/arrow-functions.ts
@@ -2,3 +2,9 @@ var greeting = (): string => {
     return "Hello";
 }
 console.log(greeting())
+
+// Passing Parameters
+var profile = (name:string): string => {
+    return "Wangutusi Arshton";
+}
+console.log(profile());
