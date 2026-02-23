19fc1d49a0171985a53179fcd8a8bd8c07cab917
wang-arshton256 <wangarshton@gmail.com>
2026-02-23 13:33:39 +0300

    updated comment

--- interfaces/functional-intefaces/functional-intefaces.ts
+++ interfaces/functional-intefaces/functional-intefaces.ts
@@ -1,5 +1,7 @@
 interface Add{
-    (x:number, y:number):void // If you dont declare a particular return type ducing interface declaration, you can assign any return type to the function. However if you declare a specifc retrurn type durng the object declaration, you must maintain it throughout.
+    (x:number, y:number):void // If you dont declare a particular return type ducing interface declaration,
+    // you can assign any return type to the function. However if you declare a specifc retrurn type durng 
+    // the object declaration, you must maintain it throughout.
 }
 
 interface Sub{
