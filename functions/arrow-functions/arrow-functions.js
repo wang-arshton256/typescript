01e03407b9a45b390d50ab697890fd713c4b7082
wang-arshton256 <wangarshton@gmail.com>
2026-02-23 11:57:16 +0300

    Array of arrow functions

--- functions/arrow-functions/arrow-functions.js
+++ functions/arrow-functions/arrow-functions.js
@@ -7,7 +7,16 @@ var profile = function (name) {
     return "Hello " + name;
 };
 console.log(profile("Wangutusi Arshton"));
+// Arrow function to calculate product
 var product = function (num1, num2) {
     return num1 * num2;
 };
 console.log("Product is", product(45, 79));
+//  Array of arrow functions
+var myarray = [];
+for (var i = 0; i < 10; i++) {
+    myarray.push(function () { return i; });
+}
+for (var i = 0; i < 10; i++) {
+    console.log(myarray[i]()); // To invoke the function use the open and close brakets.
+}
--- functions/arrow-functions/arrow-functions.ts
+++ functions/arrow-functions/arrow-functions.ts
@@ -9,8 +9,19 @@ var profile = (name:string): string => {
 }
 console.log(profile("Wangutusi Arshton"));
 
-
+// Arrow function to calculate product
 var product = (num1:number, num2:number): number => {
     return num1 * num2;
 }
 console.log("Product is", product(45, 79));
+
+//  Array of arrow functions
+var myarray: Array<any> = [];
+
+for (var i = 0; i < 10; i++) {
+    myarray.push(():number => { return i });
+}
+
+for (var i = 0; i < 10; i++){
+    console.log(myarray[i]()); // To invoke the function use the open and close brakets.
+}
