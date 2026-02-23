aa681ccd95d83541461ccbfc11351562b8d035e7
wang-arshton256 <wangarshton@gmail.com>
2026-02-23 12:25:18 +0300

    let varibale scoe

---
+++ variables/varibale-scopes/const/let.js
@@ -0,0 +1,6 @@
+// Const ensures that once a variable is assigbned a value, it can not be changed.
+// However Typescript does not stricktly enforece const like in other programming languages
+// because ES6 supported in by all WEB BROWSERS.
+var pi = 3.14;
+pi = 45;
+console.log(pi);
