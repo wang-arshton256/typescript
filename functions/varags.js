2d9685c325b8816ebc590d8e7db224ee1aecd52f
wang-arshton256 <wangarshton@gmail.com>
2026-02-23 11:16:49 +0300

    updated variable name

---
+++ functions/varags.js
@@ -0,0 +1,27 @@
+var product = function () {
+    var nums = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        nums[_i] = arguments[_i];
+    }
+    var result = 1;
+    for (var i = 0; i < nums.length; i++) {
+        console.log(nums[i]);
+        result *= nums[i];
+    }
+    return result;
+};
+console.log(product(2, 3, 4, 5));
+// Rest params
+var product2 = function (x, y) {
+    var nums = [];
+    for (var _i = 2; _i < arguments.length; _i++) {
+        nums[_i - 2] = arguments[_i];
+    }
+    var result = 1;
+    for (var i = 0; i < nums.length; i++) {
+        console.log(nums[i]);
+        result *= nums[i];
+    }
+    return result;
+};
+console.log(product(2, 3, 4, 5));
---
+++ functions/varags.ts
@@ -0,0 +1,23 @@
+var product = function (...nums) {
+    var result = 1;
+
+    for (var i = 0; i < nums.length; i++) {
+        console.log(nums[i]);
+        result *= nums[i];
+    }
+    return result;
+}
+console.log(product(2, 3, 4, 5));
+
+
+// Rest params
+var product2 = function (x:number, y:number, ...nums:number[]) {
+    var result = 1;
+
+    for (var i = 0; i < nums.length; i++) {
+        console.log(nums[i]);
+        result *= nums[i];
+    }
+    return result;
+}
+console.log(product(2, 3, 4, 5));
