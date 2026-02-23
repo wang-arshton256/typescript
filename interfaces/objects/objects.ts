9f75847e2a4d55e7e83847f3398a38e0b0bc75f2
wang-arshton256 <wangarshton@gmail.com>
2026-02-23 13:13:01 +0300

    optional parameters

--- interfaces/objects/objects.ts
+++ interfaces/objects/objects.ts
@@ -2,7 +2,7 @@ interface Product{
     id: number;
     name: string;
     description: string;
-    price: number;
+    price?: number;  // To create optional parameters, use the "?" while creating the object
 }
 
 var product1:Product = {
@@ -16,4 +16,12 @@ var product2:Product = {
     name: "Gucci Bag",
     description: "Classic leather made",
     price: 120000,
+}
+
+//
+var product2:Product = {
+    id: 123,
+    name: "Gucci Bag",
+    description: "Classic leather made"
+  
 }
