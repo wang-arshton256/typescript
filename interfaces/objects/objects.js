ad1b2aa5c3a0205a0757eb196da5652ccb2df1b1
wang-arshton256 <wangarshton@gmail.com>
2026-02-23 13:07:46 +0300

    objects

---
+++ interfaces/objects/objects.js
@@ -0,0 +1,12 @@
+var product1 = {
+    id: 123,
+    name: "Gucci Bag",
+    description: "Classic leather made",
+    price: 120000,
+};
+var product2 = {
+    id: 123,
+    name: "Gucci Bag",
+    description: "Classic leather made",
+    price: 120000,
+};
---
+++ interfaces/objects/objects.ts
@@ -0,0 +1,19 @@
+interface Product{
+    id: number;
+    name: string;
+    description: string;
+    price: number;
+}
+
+var product1:Product = {
+    id: 123,
+    name: "Gucci Bag",
+    description: "Classic leather made",
+    price: 120000,
+}
+var product2:Product = {
+    id: 123,
+    name: "Gucci Bag",
+    description: "Classic leather made",
+    price: 120000,
+}
