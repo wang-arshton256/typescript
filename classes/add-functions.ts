532e6f5a285349311796202a59169b7b097fadd6
wang-arshton256 <wangarshton@gmail.com>
2026-02-25 11:48:09 +0300

    add functions to classes

--- classes/add-functions.ts
+++ classes/add-functions.ts
@@ -1,15 +1,22 @@
-var Passenger = /** @class */ (function () {
-    function Passenger(firstName, lastName, frequentFlyerNo) {
+class Passenger {
+    firstName: string;
+    lastName: string;
+    frequentFlyerNo: number;
+    
+    constructor(firstName: string, lastName: string, frequentFlyerNo: number) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.frequentFlyerNo = frequentFlyerNo;
     }
-    Passenger.prototype.display = function () {
-        console.log(this.firstName + " " + this.lastName + " " + this.frequentFlyerNo);
-    };
-    return Passenger;
-}());
+
+    display() {
+        console.log(this.firstName+" "+this.lastName+" "+this.frequentFlyerNo);
+    }
+}
+
 var passenger1 = new Passenger("Wangutusi", "Arshton", 4587);
 passenger1.display();
+
 var passenger2 = new Passenger("Walyemwa", "Derick", 4587);
 passenger2.display();
+
