2623e5e98dfeb47b0a27304076eb97ead5701fa3
wang-arshton256 <wangarshton@gmail.com>
2026-02-26 13:07:09 +0300

    inheritance

---
+++ classes/inheritance/inheritannce.js
@@ -0,0 +1,76 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var BMW = /** @class */ (function () {
+    function BMW(make, model, year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+    BMW.prototype.commonEngineFunc = function () {
+        console.log("Common Func");
+    };
+    BMW.prototype.start = function () {
+        console.log("start");
+    };
+    BMW.prototype.stop = function () {
+        console.log("stop");
+    };
+    return BMW;
+}());
+var ThreeSeries = /** @class */ (function (_super) {
+    __extends(ThreeSeries, _super);
+    function ThreeSeries(make, model, year, cruiseControlEnabled) {
+        var _this = _super.call(this, make, model, year) || this;
+        _this.cruiseControlEnabled = cruiseControlEnabled;
+        return _this;
+    }
+    ThreeSeries.prototype.cruiseControl = function () {
+        this.cruiseControlEnabled = true;
+        console.log("Cruising");
+    };
+    return ThreeSeries;
+}(BMW));
+var FiveSeries = /** @class */ (function (_super) {
+    __extends(FiveSeries, _super);
+    function FiveSeries(make, model, year, parkingAssistEnabled) {
+        var _this = _super.call(this, make, model, year) || this;
+        _this.parkingAssistEnabled = parkingAssistEnabled;
+        return _this;
+    }
+    FiveSeries.prototype.parkingAssist = function () {
+        this.parkingAssistEnabled = true;
+        console.log("Parking");
+    };
+    return FiveSeries;
+}(BMW));
+var threeSeries = new ThreeSeries("BMW", "328I", "2018", false);
+console.log(threeSeries.cruiseControlEnabled);
+console.log(threeSeries.make);
+console.log(threeSeries.model);
+console.log(threeSeries.year);
+threeSeries.commonEngineFunc();
+threeSeries.start();
+threeSeries.stop();
+threeSeries.cruiseControl();
+var fiveSeries = new FiveSeries("BMW", "328I", "2018", false);
+console.log(fiveSeries.parkingAssistEnabled);
+console.log(fiveSeries.make);
+console.log(fiveSeries.model);
+console.log(fiveSeries.year);
+fiveSeries.commonEngineFunc();
+fiveSeries.start();
+fiveSeries.stop();
+fiveSeries.parkingAssist();
---
+++ classes/inheritance/inheritannce.ts
@@ -0,0 +1,73 @@
+class BMW{
+    make: string;
+    model: string;
+    year: string;
+
+    constructor (make: string, model: string, year: string) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+
+    commonEngineFunc() {
+        console.log("Common Func");
+    }
+
+    start() {
+        console.log("start");
+    }
+
+
+    stop() {
+        console.log("stop");
+    }
+}
+class ThreeSeries extends BMW{
+    cruiseControlEnabled: boolean;
+
+    cruiseControl() {
+        this.cruiseControlEnabled = true;
+        console.log("Cruising");
+    }
+    
+    constructor(make: string, model: string, year: string, cruiseControlEnabled: boolean) {
+        super(make, model, year);
+        this.cruiseControlEnabled = cruiseControlEnabled;
+    }
+}
+
+
+class FiveSeries extends BMW {
+    parkingAssistEnabled: boolean;
+
+    constructor(make: string, model: string, year: string, parkingAssistEnabled: boolean) {
+        super(make, model, year);
+        this.parkingAssistEnabled = parkingAssistEnabled;
+    }
+
+    parkingAssist() {
+        this.parkingAssistEnabled = true;  
+        console.log("Parking");
+    }
+}
+
+var threeSeries = new ThreeSeries("BMW", "328I", "2018", false);
+console.log(threeSeries.cruiseControlEnabled);
+console.log(threeSeries.make);
+console.log(threeSeries.model);
+console.log(threeSeries.year);
+threeSeries.commonEngineFunc();
+threeSeries.start();
+threeSeries.stop();
+threeSeries.cruiseControl();
+
+
+var fiveSeries = new FiveSeries("BMW", "328I", "2018", false);
+console.log(fiveSeries.parkingAssistEnabled);
+console.log(fiveSeries.make);
+console.log(fiveSeries.model);
+console.log(fiveSeries.year);
+fiveSeries.commonEngineFunc();
+fiveSeries.start();
+fiveSeries.stop();
+fiveSeries.parkingAssist() 
