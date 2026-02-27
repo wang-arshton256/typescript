1585a91d92aa8b56551022078975fc1e40163d0c
wang-arshton256 <wangarshton@gmail.com>
2026-02-27 13:17:00 +0300

    date method

---
+++ map&set/date/date.ts
@@ -0,0 +1,9 @@
+let expiryDate: any = new Date();
+console.log(expiryDate);
+
+let expiryDate1: any = new Date("2025-12-30"); // To set a particula date, pass the values needed
+console.log(expiryDate1.getDay()) // To get the current day of the week beginging from index 0 which is sunday to index 6
+console.log(expiryDate1.getDate()) // To get current date 
+console.log(expiryDate1.getHours()) //  To get current time in hours
+console.log(expiryDate1.getMinutes()) // To get current time in minutes
+console.log(expiryDate1.toTimeString()) // To get current time and pas it as a string
