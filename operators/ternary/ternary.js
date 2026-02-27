c93ef52b3bd0e3471cbed21630f30fef3dd7d25e
wang-arshton256 <wangarshton@gmail.com>
2026-02-27 13:40:30 +0300

    tenary operators

---
+++ operators/ternary/ternary.js
@@ -0,0 +1,3 @@
+var x = 8;
+var y = 54;
+console.log((x > y) ? "X is greater than Y" : "Y is greater than X");
---
+++ operators/ternary/ternary.ts
@@ -0,0 +1,4 @@
+var x: number = 8;
+var y: number = 54;
+
+console.log((x > y) ? "X is greater than Y" : "Y is greater than X");
